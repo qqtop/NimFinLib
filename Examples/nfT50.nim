c0435ec7e3109347a6dc28abe443d3d6506d6f38
qqTop <nomail@nomail.comp>
2017-05-06 22:01:36 +0800

    Tiny improvements

--- Examples/nfT50.nim
+++ Examples/nfT50.nim
@@ -1,13 +1,19 @@
 import cx,nimFinLib
 
+# nfT50.nim
 # Example program for using stock and index display routines
-# Run in a full terminal window size 80 x 40 column/rows
-# Font      : monospace 
-# Font Size : 9
-
+# needs to be run in a full terminal window
+# best results in bash konsole with
+# font : hack
+# font : size 8
+# background color black
+# 
 cleanscreen()
+curset()
+curdn(1)
 showCurrentIDX("^HSI+^GSPC+^FTSE+^GDAXI+^FCHI+^N225+^JKSE",xpos = 5 ,header = true)
-decho(2)
-showCurrentSTX("SNP+BP.L+0001.HK+AAPL+0027.HK+BAS.DE+MIE1.F",xpos = 5,header = true)
+curset()
+curdn(1)
+showCurrentSTX("IBM+BP.L+0001.HK+0027.HK+SNP+AAPL+BAS.DE",xpos = 72,header = true)
 curdn(5)
-doFinish()   
+doFinish()  
--- README.md
+++ README.md
@@ -14,21 +14,23 @@ Example screen from nfT50.nim
 
 ```nimrod        
 
-import cx,nimFinLib
-
 # nfT50.nim
 # Example program for using stock and index display routines
 # needs to be run in a full terminal window
-
+# best results in bash konsole with
+# font : hack
+# font : size 8
+# background color black
+# 
 cleanscreen()
 curset()
 curdn(1)
 showCurrentIDX("^HSI+^GSPC+^FTSE+^GDAXI+^FCHI+^N225+^JKSE",xpos = 5 ,header = true)
 curset()
-curup(1)
-showCurrentSTX("IBM+BP.L+0001.HK+0027.HK+AAPL+BAS.DE+MIE1.F",xpos = 72,header = true)
+curdn(1)
+showCurrentSTX("IBM+BP.L+0001.HK+0027.HK+SNP+AAPL+BAS.DE",xpos = 72,header = true)
 curdn(5)
-doFinish()  
+doFinish()   
 
 ```
 
